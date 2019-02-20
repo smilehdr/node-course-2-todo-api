9394508a4c1e96a364e780fb02aa794f51828ba2
Hendra <smilehdr@gmail.com>
2019-02-20 09:55:08 +0700

    Add test for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,18 @@ const request = require('supertest');
 const  {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text:'First todos'
+}, {
+    text: 'Second todos'
+}];
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => {
         done();
-    })
+    });
 });
 
 describe('POST/todos', () => {
@@ -25,7 +33,7 @@ describe('POST/todos', () => {
                 if(err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -45,11 +53,23 @@ describe('POST/todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
                 });
             });
+    });
+});
+
+describe('GET /todos',() => {
+    it('Should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);    
     });
 });
