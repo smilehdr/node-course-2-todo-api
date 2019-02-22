51a4d43e04c0c680e49aab77e3ddb2c2f5b09ddc
Hendra <smilehdr@gmail.com>
2019-02-22 09:12:16 +0700

    Setup separate Test and Development ENV

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-//mongoose.openUri('mongodb://todoappuser:jGNElgQDDErVMfIV@cluster0-shard-00-00-ihf3w.gcp.mongodb.net:27017,cluster0-shard-00-01-ihf3w.gcp.mongodb.net:27017,cluster0-shard-00-02-ihf3w.gcp.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
 module.exports = {mongoose};
